dd84e177eeeb44aa5f739f39780d15f4965bb4c5
renatof4rias <renatof4rias@gmail.com>
2024-05-02 12:48:21 -0300

    Desafio 0

---
+++ src/Desafio06/Reajuste.java
@@ -0,0 +1,19 @@
+package Desafio06;
+/*
+6 - Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.
+*/
+
+import java.util.Scanner;
+
+public class Reajuste {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe um Valor -> ");
+        double valor = sc.nextDouble();
+        double resultado = valor + (valor * 0.05);
+
+        System.out.println("Valor Informado com Reajuste de 5% = " + resultado);
+        sc.close();
+    }
+}
